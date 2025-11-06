03dd319ebaca3be6cd2b89ec2fe0a3313661079a
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-11-07 00:35:42 +0500

    post book added

---
+++ data/books.py
@@ -0,0 +1,31 @@
+# data/books.py
+
+books = [
+    {
+        "id": 1,
+        "title": "The Silent Ocean",
+        "author": "John Smith",
+        "publisher": "BlueWave Press",
+        "publish_date": "2020-05-15",
+        "page_count": 320,
+        "language": "English"
+    },
+    {
+        "id": 2,
+        "title": "Whispers of the Desert",
+        "author": "Aisha Khan",
+        "publisher": "Sahara Publications",
+        "publish_date": "2021-07-10",
+        "page_count": 250,
+        "language": "Urdu"
+    },
+    {
+        "id": 3,
+        "title": "The Quantum Mind",
+        "author": "Dr. Alex Turner",
+        "publisher": "FutureTech Books",
+        "publish_date": "2023-02-28",
+        "page_count": 410,
+        "language": "English"
+    }
+]
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from fastapi import FastAPI
 from pydantic import BaseModel
+from data.books import books  # 👈 import your data
 app = FastAPI()
 
 
@@ -27,4 +28,33 @@ def create_item(item: Item):
     return {
         "message": "Item created successfully!",
         "item_data": item
+    }
+
+# -----------------------CRUD on books----------------------
+
+
+
+@app.get("/books")
+def get_books():
+    return {
+        "result": books
+    }
+
+
+class Book(BaseModel):
+    title: str
+    author:str
+    publisher: str
+    publish_date: str
+    page_count: int
+    language: str
+
+
+@app.post("/books")
+def create_book(book:Book):
+    new_book = book.model_dump()
+    new_book["id"] = len(books) +1 
+    books.append(new_book)
+    return {
+        "result" : books
     }

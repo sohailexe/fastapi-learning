ebe29ab7613e10d633f37f230ac5d54000357fd4
Sohail Faiz <div.sohailfaiz@gmail.com>
2025-11-06 23:52:32 +0500

    post request added

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 
 from fastapi import FastAPI
-
+from pydantic import BaseModel
 app = FastAPI()
 
 
@@ -12,3 +12,19 @@ async def read_root():
 @app.get("/greet/{name}")
 async def read_item(name: str , age:int =0):
     return {"Hello": name, "age": age}
+
+
+# post request 
+# Step 1: Define a data model
+class Item(BaseModel):
+    name: str
+    price: float
+    in_stock: bool = True  # optional, default value
+
+# Step 2: Create POST endpoint
+@app.post("/items/")
+def create_item(item: Item):
+    return {
+        "message": "Item created successfully!",
+        "item_data": item
+    }
